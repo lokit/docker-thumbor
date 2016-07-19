122a3c5c5e1d1d9a7190174f12e44a2f568a94d4
Brais Gabín <braisgabin@gmail.com>
2016-07-19 16:58:36 +0200

    Scape the values

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,8 @@ if [ $1 == "thumbor" ]; then
 	egrep '^#[A-Z_]+ = .*' /etc/thumbor.conf | sed 's/^#\([A-Z_]*\).*$/\1/' \
 		| while read x; do
 			if [ "${!x+set}" = set ]; then
-				sed -i "s/#${x}.*/${x} = ${!x}/g" /etc/thumbor.conf
+				value=`echo ${!x} | sed -e 's/\([\\\\\/&]\)/\\\\\1/g'`
+				sed -i "s/#${x}.*/${x} = ${value}/g" /etc/thumbor.conf
 			fi
 		done
 fi
